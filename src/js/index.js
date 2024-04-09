491fcd33216208abd6a6693334211eb4cd39d856
Shu <165949343+AqueleShu@users.noreply.github.com>
2024-04-09 17:00:06 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    mostrarMaisProjetos();
+    removerBotao();
+} );
+
+function removerBotao() {
+    botaoMostrarProjetos.classList.add("remover");
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+}
